0e34b2bdd7f2aad38ba9bd9c82624a6480279e55
Scott Crowder <scrowder@gmail.com>
2024-07-08 13:30:56 -0500

    Initial Commit

---
+++ DragToSelect/DragToSelectApp.swift
@@ -0,0 +1,20 @@
+//
+// File: DragToSelectApp.swift
+// Project: DragToSelect
+// 
+// Created by SCOTT CROWDER on 7/8/24.
+// 
+// Copyright © Playful Logic Studios, LLC 2024. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct DragToSelectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
